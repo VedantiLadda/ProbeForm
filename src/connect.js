a5f44ec49a5f1165711d6e79ecf3ae7e3d3d5127
VedantiLadda <vedanti.ladda@gmail.com>
2019-05-16 10:25:42 +0530

    login card

--- src/connect.js
+++ src/connect.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as allActions from './client/actions';
+import * as allActions from './actions';
 import Root from "./router";
 
 
