53fbdc0981969c3a4cb4c0666f403955af1dac7a
vaishnavirao7 <vaishnurao@gmail.com>
2019-05-16 11:52:31 +0530

    Added modal

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
-import React, {Fragment} from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './features/login/components/LoginCard';
+import Modal from './sharedComponents/Modal/index';
 
-export default function Router(){
-    return(<BrowserRouter>
-    <Route exact path='/' component={Login}/>
+export default function Router() {
+    return (<BrowserRouter>
+        <Route exact path='/' component={Login} />
+
+        <Route exact path='/modal' component={Modal} />
     </BrowserRouter>
-        
+
     )
 }
