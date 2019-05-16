76a3a0898de9d43eeed792132db81a69f390f699
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-05-16 10:31:15 +0530

    ProfileCard

---
+++ src/features/dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../sharedComponents/Navbar';
+import ProfileCard from './components/ProfileCard';
+
+const Dashboard = () => {
+    return (
+        <>
+            <Navbar/>
+            <ProfileCard/>
+        </>
+    )
+}
+
+export default Dashboard;
