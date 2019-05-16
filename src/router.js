d214a9786d960de8c30cf201190c9d2c02444c4e
Sammed Ravanavar <33900745+sammedravanavar@users.noreply.github.com>
2019-05-16 12:22:45 +0530

    Profile Card built

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import React, {Fragment} from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
 import Login from './features/login/components/LoginCard';
-
+import Dashboard from './features/dashboard';
 export default function Router(){
-    return(<BrowserRouter>
-    <Route exact path='/' component={Login}/>
-    </BrowserRouter>
-        
+    return(
+        <BrowserRouter>
+            <Route exact path='/' component={Login}/>
+            <Route exact path='/dashboard' component={Dashboard}/>
+        </BrowserRouter>
     )
 }
